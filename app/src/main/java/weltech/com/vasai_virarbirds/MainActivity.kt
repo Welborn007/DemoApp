234b9b6a589918503ad954ddd9435148c9649fe0
Welborn Machado <welbornm@moofwd.com>
2022-03-05 12:07:15 +0530

    Initial Setup

---
+++ app/src/main/java/weltech/com/vasai_virarbirds/MainActivity.kt
@@ -0,0 +1,11 @@
+package weltech.com.vasai_virarbirds
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
